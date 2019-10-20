483705c82ff1cf2858ad8e95d6334fdbe1bcdf5b
Jakub Sitnicki <jakub@cloudflare.com>
2019-10-20 13:23:44 +0200

    BACKPORT: scripts/bpf: Print an error when known types list needs updating
    Don't generate a broken bpf_helper_defs.h header if the helper script needs
    updating because it doesn't recognize a newly added type. Instead print an
    error that explains why the build is failing, clean up the partially
    generated header and stop.
    
    v1->v2:
    - Switched from temporary file to .DELETE_ON_ERROR.
    
    Fixes: 456a513bb5d4 ("scripts/bpf: Emit an #error directive known types list needs updating")
    Suggested-by: Andrii Nakryiko <andriin@fb.com>
    Change-Id: I57f30d2d4e346c0d9237afc67a1d9d5c908d681d
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yonghong Song <yhs@fb.com>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20191020112344.19395-1-jakub@cloudflare.com
    (cherry picked from commit ab81e203bc0d4a4542bca2498535f8761a3cfd92)

--- scripts/bpf_helpers_doc.py
+++ scripts/bpf_helpers_doc.py
@@ -489,7 +489,8 @@ class PrinterHelpers(Printer):
         if t in self.mapped_types:
             return self.mapped_types[t]
         print("")
-        print("Unrecognized type '%s', please add it to known types!" % t)
+        print("Unrecognized type '%s', please add it to known types!" % t,
+              file=sys.stderr)
         sys.exit(1)
 
     seen_helpers = set()
