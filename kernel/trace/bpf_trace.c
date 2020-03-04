c0991dbe68a8ea2d81d89f50d7d681f2f9f9a980
Carlos Neira <cneirabustos@gmail.com>
2020-03-04 17:41:56 -0300

    bpf: Added new helper bpf_get_ns_current_pid_tgid
    New bpf helper bpf_get_ns_current_pid_tgid,
    This helper will return pid and tgid from current task
    which namespace matches dev_t and inode number provided,
    this will allows us to instrument a process inside a container.
    
    Change-Id: I76919b6824cd01b039788a274b671b67e156055f
    Signed-off-by: Carlos Neira <cneirabustos@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/bpf/20200304204157.58695-3-cneirabustos@gmail.com

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -738,6 +738,8 @@ tracing_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 #endif
 	case BPF_FUNC_send_signal:
 		return &bpf_send_signal_proto;
+	case BPF_FUNC_get_ns_current_pid_tgid:
+		return &bpf_get_ns_current_pid_tgid_proto;
 	default:
 		return NULL;
 	}
