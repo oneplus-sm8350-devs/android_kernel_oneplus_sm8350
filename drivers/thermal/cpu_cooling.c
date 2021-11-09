06068b59bb75342b77f43d75a14d3983d060c49b
Lukasz Luba <lukasz.luba@arm.com>
2021-11-09 19:57:11 +0000

    thermal: cpu_cooling: Use new thermal pressure update function
    Thermal pressure provides a new API, which allows to use CPU frequency
    as an argument. That removes the need of local conversion to capacity.
    Use this new function and remove old conversion code.
    
    Signed-off-by: Lukasz Luba <lukasz.luba@arm.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -473,7 +473,6 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 {
 	struct cpufreq_cooling_device *cpufreq_cdev = cdev->devdata;
 	struct cpumask *cpus;
-	unsigned long max_capacity, capacity;
 	unsigned int frequency;
 	int ret;
 
@@ -492,10 +491,7 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 	if (ret >= 0) {
 		cpufreq_cdev->cpufreq_state = state;
 		cpus = cpufreq_cdev->policy->related_cpus;
-		max_capacity = arch_scale_cpu_capacity(cpumask_first(cpus));
-		capacity = frequency * max_capacity;
-		capacity /= cpufreq_cdev->policy->cpuinfo.max_freq;
-		arch_set_thermal_pressure(cpus, max_capacity - capacity);
+		arch_update_thermal_pressure(cpus, frequency);
 		ret = 0;
 	}
 
