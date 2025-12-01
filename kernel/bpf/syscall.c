6cfcb38e3df91ed94f69bdae5e6884e80c03b402
basamaryan <basam.aryan@gmail.com>
2025-11-30 23:29:19 -0800

    squashme
    Change-Id: I9be3c9f3de8c1d49f9d7310a68fa2bdb2811d160

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -316,12 +316,12 @@ static void *__bpf_map_area_alloc(u64 size, int numa_node, bool mmapable)
 					   flags, __builtin_return_address(0));
 }
 
-void *bpf_map_area_alloc(size_t size, int numa_node)
+void *bpf_map_area_alloc(u64 size, int numa_node)
 {
 	return __bpf_map_area_alloc(size, numa_node, false);
 }
 
-void *bpf_map_area_mmapable_alloc(size_t size, int numa_node)
+void *bpf_map_area_mmapable_alloc(u64 size, int numa_node)
 {
 	return __bpf_map_area_alloc(size, numa_node, true);
 }
