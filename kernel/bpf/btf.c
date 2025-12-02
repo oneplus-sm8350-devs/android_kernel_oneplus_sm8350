93d15f1367b21ff31a1eeec4d9e2067c48fbfd0f
basamaryan <basam.aryan@gmail.com>
2025-12-02 14:15:43 -0800

    TMP
    till https://review.lineageos.org/c/LineageOS/android_kernel_qcom_sm8250/+/456773/1
    
    Change-Id: Ib13acd46dcbc16938c2df3cd4b638ee725855fac

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -1105,8 +1105,8 @@ static void *btf_show_obj_safe(struct btf_show *show,
 		size_left = btf_show_obj_size_left(show, data);
 		if (size_left > BTF_SHOW_OBJ_SAFE_SIZE)
 			size_left = BTF_SHOW_OBJ_SAFE_SIZE;
-		show->state.status = copy_from_kernel_nofault(show->obj.safe,
-							      data, size_left);
+		show->state.status = probe_kernel_read(show->obj.safe,
+						data, size_left);
 		if (!show->state.status) {
 			show->obj.data = data;
 			safe = show->obj.safe;
