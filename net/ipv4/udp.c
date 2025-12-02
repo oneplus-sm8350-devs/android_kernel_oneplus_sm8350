3ce06b421f4dea568840f8f40789feff35dbdddd
basamaryan <basam.aryan@gmail.com>
2025-12-02 15:14:49 -0800

    fixup! BACKPORT: net/udp: switch udp_lib_setsockopt to sockptr_t

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -2731,7 +2731,7 @@ int compat_udp_setsockopt(struct sock *sk, int level, int optname,
 			  char __user *optval, unsigned int optlen)
 {
 	if (level == SOL_UDP  ||  level == SOL_UDPLITE)
-		return udp_lib_setsockopt(sk, level, optname, optval, optlen,
+		return udp_lib_setsockopt(sk, level, optname, USER_SOCKPTR(optval), optlen,
 					  udp_push_pending_frames);
 	return compat_ip_setsockopt(sk, level, optname, optval, optlen);
 }
